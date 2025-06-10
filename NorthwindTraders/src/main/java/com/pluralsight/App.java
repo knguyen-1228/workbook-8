4a2fa4378064df793dd78578e0f6a14493b68fcb
Kevin Nguyen <knguyen@appdev.yearup.org>
2025-06-10 11:13:10 -0500

    created a login and used finally in try catch

--- NorthwindTraders/src/main/java/com/pluralsight/App.java
+++ NorthwindTraders/src/main/java/com/pluralsight/App.java
@@ -1,32 +1,96 @@
 package com.pluralsight;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) {
-        // 1. open a connection to the database
-// use the database URL to point to the correct database
-        if(args.length != 2){
-            System.out.println("Application needs two arguments to run" + "java com.pluralsight.UsingDriverManager <username><password>");
-            System.exit(1);
-        }
-        String username = args[0];
-        String password = args[1];
-        try {
-            Connection connection;
-            connection = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/northwind", username, password);
 
+    static Scanner myScanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        Connection connection = login();
+
+        boolean homeRunning = true;
+        while (homeRunning) {
+            int homeChoice = homeScreen();
+            switch (homeChoice) {
+                case 1:
+                    viewProducts(connection);
+                    break;
+                case 2:
+                    viewCustomer(connection);
+                    break;
+                case 0:
+                    System.out.println("Exiting the app");
+                    homeRunning = false;
+                    break;
+            }
+        }
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+
+    public static Connection login() {
+
+        Connection connection = null;
+
+
+        while (true) {
+            System.out.println("Please enter your username");
+            String username = myScanner.nextLine();
+            System.out.println("Please enter your password");
+            String password = myScanner.nextLine();
+            try {
+                connection = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/northwind", username, password);
+                System.out.println("Login Successful!");
+                return connection;
+
+            } catch (SQLException e) {
+                System.out.println("Login failed, please try again!");
+
+            }
+        }
+
+
+    }
+
+    public static int homeScreen() {
+        int choice;
+        System.out.println("What do you want to do?");
+        System.out.println("\t1) Display all products");
+        System.out.println("\t2) Display all customers");
+        System.out.println("\t0) Exit");
+        System.out.println("Select an option");
+        choice = myScanner.nextInt();
+        myScanner.nextLine();
+
+        return choice;
+
+    }
+
+    public static void viewProducts(Connection connection) {
+        PreparedStatement pStatement = null;
+        ResultSet results = null;
+        try {
 
             // create statement
             // the statement is tied to the open connection
-            PreparedStatement pStatement = connection.prepareStatement(
+            pStatement = connection.prepareStatement(
                     "SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM products"
             );
 
 
             // 2. Execute your query
-            ResultSet results = pStatement.executeQuery();
+            results = pStatement.executeQuery();
 
 
             // process the results
@@ -44,14 +108,78 @@ public class App {
             }
 
 
-            // 3. Close the connection
-            results.close();
-            pStatement.close();
-            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            // close the resources
+            if (results != null) {
+                try {
+                    results.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (pStatement != null) {
+                try {
+                    pStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void viewCustomer(Connection connection) {
+        PreparedStatement pStatement = null;
+        ResultSet results = null;
+        try {
+
+            // create statement
+            // the statement is tied to the open connection
+            pStatement = connection.prepareStatement(
+                    "SELECT ContactName, CompanyName, City, Country, Phone FROM customer"
+            );
+
+
+            // 2. Execute your query
+            results = pStatement.executeQuery();
+
+
+            // process the results
+            while (results.next()) {
+                String name = results.getString("ContactName");
+                String companyName = results.getString("CompanyName");
+                String city = results.getString("City");
+                String country = results.getString("Country");
+                String phone = results.getString("Phone");
+
+                System.out.println("Contact Name: " + name);
+                System.out.println("Company Name: " + companyName);
+                System.out.println("City: " + city);
+                System.out.println("Country:  " + country);
+                System.out.println("Phone Number:  " + phone);
+                System.out.println("-------------------------------------------------");
+            }
+
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        } finally {
+            // close the resources
+            if (results != null) {
+                try {
+                    results.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (pStatement != null) {
+                try {
+                    pStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
-
     }
 }
